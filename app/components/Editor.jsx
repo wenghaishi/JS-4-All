305dd103beef4b8b064c648ebe7e67a83c38d42e
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-02 14:23:28 +0800

    implemented feature to evaluate user code in editor

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -1,15 +1,36 @@
 "use client";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const CodeEditor = dynamic(() => import("@monaco-editor/react"), {
+  ssr: false,
+});
 
 function Editor({ code, language, onChange }) {
-  const CodeEditor = dynamic(() => import("@monaco-editor/react"), {
-    ssr: false,
-  });
+  const [userCode, setUserCode] = useState("");
+  const [output, setOutput] = useState("");
 
   const editorOptions = {
     selectOnLineNumbers: true,
     fontSize: 14,
     automaticLayout: true,
+  };
+
+  const handleEditorChange = (value) => {
+    setUserCode(value);
+  };
+
+  const runCode = () => {
+    try {
+      let evaluatedCode = eval(userCode); // Evaluate the user's code
+      if (Array.isArray(evaluatedCode)) {
+        evaluatedCode = "[" + evaluatedCode.join(", ") + "]";
+        evaluatedCode = evaluatedCode.replace(" ", "");
+      }
+      setOutput(evaluatedCode);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -21,15 +42,19 @@ function Editor({ code, language, onChange }) {
         theme="vs-dark"
         defaultValue={`${code}`}
         options={editorOptions}
-        // value={value}
-        // onChange={handleEditorChange}
-        // theme={theme}
+        value={userCode}
+        onChange={handleEditorChange}
       />
       <div className="flex flex-row items-center justify-between mt-3 w-10/12">
-        <h3 className="border border-neutral-50/20 w-full mr-3 px-6 py-4 text-slate-100">Output:</h3>
-        <button className="bg-white px-16 py-4 rounded-lg">Run</button>
+        <h3 className="border border-neutral-50/20 w-full mr-3 tracking-widest px-6 py-4 text-slate-100">
+          Output: {output && output}
+        </h3>
+        <button onClick={runCode} className="bg-white px-16 py-4 rounded-lg">
+          Run
+        </button>
       </div>
     </>
   );
 }
+
 export default Editor;
