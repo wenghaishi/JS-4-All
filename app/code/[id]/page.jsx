aea7a00455bf2ce6516a36bc0735a59803cad3b1
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-03 12:00:01 +0800

    added markers for question description and function to modify marked texts

--- app/code/[id]/page.jsx
+++ app/code/[id]/page.jsx
@@ -1,12 +1,28 @@
-import Editor from "@/app/components/Editor"
-async function page( { params: { id } }) {
-  const question = await (await fetch(`http://localhost:3000/api/questions/${id}`)).json()
-  console.log(question.name)
+import Editor from "@/app/components/Editor";
+
+async function page({ params: { id } }) {
+  const question = await (
+    await fetch(`http://localhost:3000/api/questions/${id}`, {
+      cache: "no-store",
+    })
+  ).json();
+
+  function replaceWordsWithSpan(description) {
+    const regex = /:(\w+)/g;
+    return description.replace(regex, '<span class="text-indigo-400">$1</span>');
+  }
+  
+  const modifiedDescription = replaceWordsWithSpan(question.description);
+
   return (
-    <div className="w-screen h-screen bg-black flex flex-col items-center pt-16">
-      <h1 className="text-white my-6 text-3xl font-black">{question.name}</h1>
+    <div className="w-screen h-screen bg-black flex flex-col items-center py-16">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-red-500 pt-10 pb-6 text-4xl font-black">
+        {question.name}
+      </h1>
+      <h2 className="text-white mb-6 tracking-wide text-lg w-10/12 text-justify" dangerouslySetInnerHTML={{ __html: modifiedDescription }} />
       <Editor code={question.code} />
     </div>
-  )
+  );
 }
-export default page
+
+export default page;
