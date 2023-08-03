c7f93bd46894fd0c7389493e5b210f9e8c6e184c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-03 15:26:50 +0800

    changed api

--- app/code/[id]/page.jsx
+++ app/code/[id]/page.jsx
@@ -1,6 +1,6 @@
 import Editor from "@/app/components/Editor"
 async function page( { params: { id } }) {
-  const question = await (await fetch(`http://localhost:3000/api/questions/${id}`)).json()
+  const question = await (await fetch(`${process.env.API}/api/questions/${id}`)).json()
   console.log(question.name)
   return (
     <div className="w-screen h-screen bg-black flex flex-col items-center pt-16">
--- lib/getAllQuestions.jsx
+++ lib/getAllQuestions.jsx
@@ -1,5 +1,5 @@
 export default async function getAllQuestions() {
-  const response = await fetch("http://localhost:3000/api/questions");
+  const response = await fetch(`${process.env.API}/api/questions`);
   // if (!response.ok) throw new Error("failed to fetched data");
   return response.json();
 }
