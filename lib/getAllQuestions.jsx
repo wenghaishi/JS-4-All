3a56e2a786eedffbfee9995e7eed587d407e2b2e
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-06 15:42:50 +0800

    debug

--- lib/getAllQuestions.jsx
+++ lib/getAllQuestions.jsx
@@ -2,10 +2,10 @@ export default async function getAllQuestions() {
   const response = await fetch(`${process.env.API}/api/questions`, { cache: 'no-store' });
   // if (!response.ok) throw new Error("failed to fetched data");
 
-  const contentType = response.headers.get("content-type");
-  if (contentType.includes("text/html")) {
-    return null; // Return null if the response is HTML
-  }
-  
-  return response.json();
+  // const contentType = response.headers.get("content-type");
+  // if (contentType.includes("text/html")) {
+  //   return null; // Return null if the response is HTML
+  // }
+
+  return response;
 }
