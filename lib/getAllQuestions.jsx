87b774b2ef691c19d6d16fc6f1fb2f1cd6da56af
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-06 15:19:16 +0800

    debug

--- lib/getAllQuestions.jsx
+++ lib/getAllQuestions.jsx
@@ -1,5 +1,5 @@
 export default async function getAllQuestions() {
-  const response = await fetch(`https://js-4-all.vercel.app/api/questions`, { cache: 'no-store' });
+  const response = await fetch(`https://js-4-all-git-editor-wenghaishi.vercel.app/api/questions`, { cache: 'no-store' });
   // if (!response.ok) throw new Error("failed to fetched data");
   return response.json();
 }
