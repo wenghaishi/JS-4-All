a30c8a9f552c473ff3c97a6ee2ccc07c10207d6b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-06 15:39:37 +0800

    debug

--- app/code/[id]/page.jsx
+++ app/code/[id]/page.jsx
@@ -2,7 +2,12 @@ import Editor from "@/app/components/Editor"
 
 async function page( { params: { id } }) {
   const question = await (await fetch(`${process.env.API}/api/questions/${id}`, { cache: 'no-store' })).json()
-  console.log(question.name)
+
+  const contentType = response.headers.get("content-type");
+  if (contentType.includes("text/html")) {
+    return null; // Return null if the response is HTML
+  }
+
   return (
     <div className="w-screen h-screen bg-black flex flex-col items-center pt-16">
       <h1 className="text-white my-6 text-3xl font-black">{question.name}</h1>
--- lib/getAllQuestions.jsx
+++ lib/getAllQuestions.jsx
@@ -1,5 +1,11 @@
 export default async function getAllQuestions() {
   const response = await fetch(`${process.env.API}/api/questions`, { cache: 'no-store' });
   // if (!response.ok) throw new Error("failed to fetched data");
+
+  const contentType = response.headers.get("content-type");
+  if (contentType.includes("text/html")) {
+    return null; // Return null if the response is HTML
+  }
+  
   return response.json();
 }
