81eb20d0234f09d2e5210bdb179bc5ae88f3eefd
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-07 10:26:34 +0800

    debug

--- lib/getAllQuestions.jsx
+++ lib/getAllQuestions.jsx
@@ -1,5 +1,6 @@
 export default async function getAllQuestions() {
   const response = await fetch(`${process.env.NEXT_API}/api/questions`, { cache: 'no-store' });
-  // if (!response.ok) throw new Error("failed to fetched data");
+  if (!response.ok) throw new Error("failed to fetched data");
+
   return response.json();
 }
