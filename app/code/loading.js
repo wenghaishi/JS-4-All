02c43f02ad20bc2f0ee99c5f9a861ee6fbea306c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-08 06:47:51 +0800

    added loading pages

---
+++ app/code/loading.js
@@ -0,0 +1,10 @@
+function loading() {
+  return (
+    <div className="bg-black text-white h-screen display flex flex-col items-center justify-center">
+      <div className="spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
+    </div>
+  );
+}
+export default loading;
