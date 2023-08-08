04aba23f08e9cea84505d316234afbacd72cf361
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-08 14:09:39 +0800

    added selection for flashcards

--- app/flashcards/javascript/page.jsx
+++ app/flashcards/javascript/page.jsx
@@ -1,30 +1,53 @@
-
-
+"use client";
+import { useEffect, useState, useRef } from "react";
 import getAllFlashcards from "@/lib/getAllFlashcards";
-async function page() {
 
-  const handleSelect = () => {
-    console.log("clicked")
-  }
-  const jsFlashcards = await getAllFlashcards()
+function Page() {
+  const [flashcards, setFlashcards] = useState([]);
+  const key = [0, 1, 2, 3];
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const jsFlashcards = await getAllFlashcards();
+      console.log(jsFlashcards.answer);
+      setFlashcards(jsFlashcards);
+    };
+    fetchData();
+  }, []);
 
+  const handleSelect = (event) => {
+    if (
+      event.currentTarget.innerHTML ==
+      flashcards[0].options[flashcards[0].answer]
+    ) {
+      event.currentTarget.style.backgroundColor = "#009E60";
+    } else {
+      event.currentTarget.style.backgroundColor = "#f94449";
+    }
+  };
 
   return (
     <div className="bg-black pt-16 h-screen text-white flex flex-col items-center justify-center">
-      <h1 className="text-white text-xl tracking-wider my-10">
-        {jsFlashcards[0].description}
-      </h1>
-      {jsFlashcards[0].options.map((option) => (
-        <div
-          // onClick={handleSelect}
-          key={option}
-          className="text-white border text-xl border-neutral-50/30 w-8/12 text-center rounded-xl mb-10 py-10"
-        >
-          {option}
-        </div>
-      ))}
+      {flashcards.length > 0 ? (
+        <>
+          <h1 className="text-white text-xl tracking-wider my-10">
+            {flashcards[0].description}
+          </h1>
+          {flashcards[0].options.map((option, index) => (
+            <div
+              onClick={handleSelect}
+              key={index}
+              className={`text-white border text-xl border-neutral-50/30 w-8/12 text-center rounded-xl mb-10 py-10`}
+            >
+              {option}
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Loading flashcards...</p>
+      )}
     </div>
   );
 }
-export default page;
+
+export default Page;
