65ce1ca3825ad0d592383d57060913cb3c08f53b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-08 12:43:32 +0800

    uncomment fetch

--- app/flashcards/javascript/page.jsx
+++ app/flashcards/javascript/page.jsx
@@ -1,13 +1,13 @@
 async function page() {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL
-  // const response = await fetch(`${apiUrl}/api/flashcards/javascript`, {cache: 'no-store'})
+  const response = await fetch(`${apiUrl}/api/flashcards/javascript`, {cache: 'no-store'})
 
-  // if (!response.ok) throw new Error("failed to fetched data");
-  // const jsFlashcards = await response.json()
-  // console.log(jsFlashcards[0].description)
+  if (!response.ok) throw new Error("failed to fetched data");
+  const jsFlashcards = await response.json()
+  console.log(jsFlashcards[0].description)
   return (
     <div className="bg-black pt-16 h-screen text-white">
-      {/* <h1 className="text-white">{jsFlashcards[0].description}</h1> */}
+      <h1 className="text-white">{jsFlashcards[0].description}</h1>
     </div>
   )
 }
