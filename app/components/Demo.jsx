9762424e465cbb63b6738836e5f57f1597beeba6
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-12 19:21:54 +0800

    chnaged test case for demo editor

--- app/components/Demo.jsx
+++ app/components/Demo.jsx
@@ -9,7 +9,8 @@ function Demo() {
   );
 
   const description = "Given an array of integers :nums and an integer :target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice."
-
+  
+  const test = ["[0,1]"]
   const handleCodeChange = (newCode) => {
     setCode(newCode);
   };
@@ -17,7 +18,7 @@ function Demo() {
     <div className="pt-10 pb-20 px-16 flex flex-col items-center  bg-black">
       <h1 className="text-white text-5xl mb-12">Try it out!</h1>
       <h2 className="mb-10 tracking-wider text-3xl w-10/12 text-center text-green-600">Two sum</h2>
-      <Editor code={code} description={description} language="javascript" onChange={handleCodeChange} />
+      <Editor code={code} description={description} test={test} language="javascript" onChange={handleCodeChange} />
     </div>
   );
 }
