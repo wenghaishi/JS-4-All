c8400de1ad0ff79ef0f657d5ba0540257bdc09fb
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-15 14:39:44 +0800

    added user page

--- app/components/indexPage/Features.jsx
+++ app/components/indexPage/Features.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 function Features() {
   return (
-    <div className="flex flex-col py-44 md:px-20 px-10 bg-black text-white items-center">
+    <div className="flex flex-col py-36 md:px-20 px-10 bg-black text-white items-center">
       <h1 className="text-5xl font-extrabold">Features</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-evenly w-full mt-10">
         <Link href="/code" className="p-12 border rounded-xl border-neutral-100/30 flex flex-col items-center justify-evenly text-center feature-card1">
--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -1,6 +1,31 @@
-function page() {
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+
+function UserPage() {
+  const { status, data: session } = useSession();
+
   return (
-    <div>page</div>
-  )
+    <div className="bg-black pt-24 pb-10 h-screen flex flex-col sm:flex-row gap-10  px-10 text-white">
+      <div className="flex flex-row items-start bg-gray-800 p-14 rounded-xl w-2/6">
+        <Image
+          src={session.user.image}
+          width={70}
+          height={70}
+          alt="user profile picture"
+        />
+        <div className="flex flex-col ml-6">
+          <h1 className="text-white md:text-lg font-bold text-sm">{session.user.email}</h1>
+          <h1 className="text-white md:text-lg font-bold text-sm">{session.user.name}</h1>
+        </div>
+      </div>
+
+      <div className="w-4/6 bg-gray-800 p-14 rounded-xl">
+        <h1 className="text-white text-lg">Flashcard completions:</h1>
+        <h1 className="text-white text-lg">Algo-question completions:</h1>
+      </div>
+    </div>
+  );
 }
-export default page
+export default UserPage;
