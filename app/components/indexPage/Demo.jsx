744b20599bf25b0d7c8ac59d1c015b45df16f14f
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-15 14:48:28 +0800

    small change to demo section

--- app/components/indexPage/Demo.jsx
+++ app/components/indexPage/Demo.jsx
@@ -9,7 +9,7 @@ function Demo() {
   );
 
   const description =
-    "Given an array of integers :nums and an integer :target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.";
+    "Given an array of integers :arr and an integer :target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.";
 
   const test = ["[0,1]"];
   const handleCodeChange = (newCode) => {
