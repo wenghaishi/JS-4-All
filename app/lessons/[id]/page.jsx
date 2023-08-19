a1f14b3c8d7a44feffb5848af3736533cbabdb22
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-08-19 16:38:53 +0800

    added padding to lessons page

--- app/lessons/[id]/page.jsx
+++ app/lessons/[id]/page.jsx
@@ -8,12 +8,12 @@ async function page({ params: { id } }) {
   const lesson = await response.json();
 
   return (
-    <div className="h-screen text-center pt-16 bg-black text-white flex flex-col items-center">
+    <div className=" text-center pt-16 bg-black text-white flex flex-col items-center">
       <h1 className="text-4xl mt-16">{lesson.name}</h1>
       {lesson.sections.map((section) => (
         <div key={section.id}>
           <h2 className="text-2xl mt-10">{section.title}</h2>
-          <p className="text-base mt-6 sm:px-10 md:px-14">{section.body}</p>
+          <p className="text-base mt-6 px-4 sm:px-10 mx-6 md:mx-12 md:px-14 bg-slate-800 py-6 md:py-10 rounded-xl">{section.body}</p>
         </div>
       ))}
     </div>
